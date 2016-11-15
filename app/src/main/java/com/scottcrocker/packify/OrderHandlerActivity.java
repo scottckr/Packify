bfe362cbdab94b4c7c2b2e2775e95e2d4c543117
samanthamorrison <samantha.morrison@hotmail.se>
2016-11-15 14:16:59 +0100

    Created temporary methods for buttons in OrderHandlerActivity and UserHandlerActivity

--- app/src/main/java/com/scottcrocker/packify/OrderHandlerActivity.java
+++ app/src/main/java/com/scottcrocker/packify/OrderHandlerActivity.java
@@ -2,6 +2,8 @@ package com.scottcrocker.packify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class OrderHandlerActivity extends AppCompatActivity {
 
@@ -9,5 +11,28 @@ public class OrderHandlerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_handler);
+    }
+
+    /**
+     * Method to create a new order object, or handle an existing order object which will be sent to DB
+     * @param view
+     */
+
+    // TO-DO: create new object containing order information, send to database
+    public void save_order(View view) {
+        Toast toast = new Toast(this);
+
+        Toast.makeText(getApplicationContext(), "Order sparad", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Method to delete order from DB
+     * @param view
+     */
+    // TO-DO: method shall delete order information in database
+    public void delete_order(View view) {
+        Toast toast = new Toast(this);
+
+        Toast.makeText(getApplicationContext(), "Order raderad", Toast.LENGTH_SHORT).show();
     }
 }
