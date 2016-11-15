4640b7672baa80c36c2ecd9ebad977abb64dc755
niklasbolwede <niklasbolwede@gmail.com>
2016-11-15 15:06:24 +0100

    Changed some names in activity_settings

--- app/src/main/java/com/scottcrocker/packify/SettingsActivity.java
+++ app/src/main/java/com/scottcrocker/packify/SettingsActivity.java
@@ -2,12 +2,21 @@ package com.scottcrocker.packify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+
     }
 }
