a92d462903ae0b55e150202e6d9aaf32603d4776
Mavve <mavve.a@gmail.com>
2016-11-15 12:33:25 +0100

    Created the layout for LoginActivity & OrderHistory and Created intents for sending the user from MainActivity to LoginActivity to ActiveOrdersActivity from the login Submit Button.

--- app/src/main/java/com/scottcrocker/packify/OrderHistoryActivity.java
+++ app/src/main/java/com/scottcrocker/packify/OrderHistoryActivity.java
@@ -1,13 +1,36 @@
 package com.scottcrocker.packify;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderHistoryActivity extends AppCompatActivity {
+
+    private ListView historyListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_history);
+
+        historyListView = (ListView) findViewById(R.id.order_history_listView);
+
+        // test to populate the ListView
+        List<String> your_array_list = new ArrayList<String>();
+        your_array_list.add("foo");
+        your_array_list.add("bar");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                your_array_list );
+
+        historyListView.setAdapter(arrayAdapter);
+
+
     }
 }
