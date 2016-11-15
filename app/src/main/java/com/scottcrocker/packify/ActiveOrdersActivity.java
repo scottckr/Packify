9fd362081d4b57a59052f6f8b1aabc139c52a158
Mavve <mavve.a@gmail.com>
2016-11-15 15:51:40 +0100

    ActiveOrders sends you to specific orderactivity with a toast for which item was selected.

--- app/src/main/java/com/scottcrocker/packify/ActiveOrdersActivity.java
+++ app/src/main/java/com/scottcrocker/packify/ActiveOrdersActivity.java
@@ -1,13 +1,13 @@
 package com.scottcrocker.packify;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import com.scottcrocker.packify.model.Order;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,14 +22,25 @@ public class ActiveOrdersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_active_orders);
 
-        List<String> listViewArray = new ArrayList<>();
+        final List<String> listViewArray = new ArrayList<>();
 
         listViewArray.add("Order 1");
         listViewArray.add("Order 2");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listViewArray);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listViewArray);
 
         listView = (ListView)findViewById(R.id.active_orders_listview);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                String item = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(),item,Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), SpecificOrderActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
