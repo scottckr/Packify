cdb5e32176a0d699179a9f9e451d28763b1fe81a
scottckr <scottckr@hotmail.com>
2016-11-15 14:27:53 +0100

    Fixed more conflicts.

--- app/src/main/java/com/scottcrocker/packify/model/Order.java
+++ app/src/main/java/com/scottcrocker/packify/model/Order.java
@@ -1,8 +1,22 @@
 package com.scottcrocker.packify.model;
 
+import java.util.Date;
+
 /**
  * Created by mavve on 2016-11-11.
  */
 
 public class Order {
+    private int customerNo;
+    private String address;
+    private int orderSum;
+    private static int orderNo;
+    private boolean delivered;
+    private Date deliveryDate;
+    private double latitude;
+    private double longitude;
+
+    public static int getOrderNo() {
+        return orderNo;
+    }
 }
