3de644732aaf244664a193f96a5e993c02073d9f
Tobias Hillén <tobias.hillen@gmail.com>
2016-11-18 13:24:58 +0100

    Fixed toolbar in all activities. TODO: Design issues with padding in most xmls.

--- app/src/main/java/com/scottcrocker/packify/OrderHistoryActivity.java
+++ app/src/main/java/com/scottcrocker/packify/OrderHistoryActivity.java
@@ -1,7 +1,11 @@
 package com.scottcrocker.packify;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,6 +20,8 @@ public class OrderHistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_history);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         historyListView = (ListView) findViewById(R.id.order_history_listview);
 
@@ -33,4 +39,34 @@ public class OrderHistoryActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.toolbar_update_order:
+                //TODO Update view.
+                return true;
+
+            case R.id.toolbar_settings:
+                intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.toolbar_admin_settings:
+                intent = new Intent(this, UserHandlerActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 }
--- app/src/main/java/com/scottcrocker/packify/UserHandlerActivity.java
+++ app/src/main/java/com/scottcrocker/packify/UserHandlerActivity.java
@@ -1,7 +1,11 @@
 package com.scottcrocker.packify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -14,15 +18,48 @@ public class UserHandlerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_handler);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         mSpinner = (Spinner) findViewById(R.id.spinner_user_id);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.toolbar_update_order:
+                //TODO Update view.
+                return true;
+
+            case R.id.toolbar_settings:
+                intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.toolbar_admin_settings:
+                intent = new Intent(this, UserHandlerActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     /**
      * Method to create a new user object, or handle an existing user object which will be sent to DB
      * @param view
      */
-    // TO-DO: create new object containing user information, send to database
+    // TODO: create new object containing user information, send to database
     public void save_user(View view) {
         Toast toast = new Toast(this);
 
@@ -33,7 +70,7 @@ public class UserHandlerActivity extends AppCompatActivity {
      * Method to delete user from DB
      * @param view
      */
-    // TO-DO: method shall delete user information in database
+    // TODO: method shall delete user information in database
     public void delete_user(View view) {
         Toast toast = new Toast(this);
 
