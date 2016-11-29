7d101d955a716d3eeb8d7c582f0fc2576c1af9f2
scottckr <scottckr@hotmail.com>
2016-11-29 16:38:02 +0100

    Changed paint color to black.

--- app/src/main/java/com/scottcrocker/packify/helper/DrawingView.java
+++ app/src/main/java/com/scottcrocker/packify/helper/DrawingView.java
@@ -21,7 +21,7 @@ public class DrawingView extends View {
 
     private Path drawPath;
     private Paint drawPaint, canvasPaint;
-    private int paintColor = 0xFF660000;
+    private int paintColor = 0x00000000;
     private Canvas drawCanvas;
     private Bitmap canvasBitmap;
 
