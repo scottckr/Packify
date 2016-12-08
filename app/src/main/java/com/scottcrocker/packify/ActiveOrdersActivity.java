81a329da3f8d611b2f8eb399eaaeaa1f704f2dab
Samantha Morrison <samanthamorrison@Apples-MacBook-Pro.local>
2016-12-08 14:01:17 +0100

    Cleaned all layout files

--- app/src/main/java/com/scottcrocker/packify/ActiveOrdersActivity.java
+++ app/src/main/java/com/scottcrocker/packify/ActiveOrdersActivity.java
@@ -16,13 +16,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.scottcrocker.packify.controller.OrderViewAdapter;
-
 import com.scottcrocker.packify.helper.RandomHelper;
 import com.scottcrocker.packify.model.Order;
 import com.scottcrocker.packify.model.User;
